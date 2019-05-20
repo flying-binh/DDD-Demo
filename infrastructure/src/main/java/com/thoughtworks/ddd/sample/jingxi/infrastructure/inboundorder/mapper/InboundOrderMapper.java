2f85c5b2fc6b97a2e58f47a3311b6a3b70248fbd
duorourou <duoorourou@gmail.com>
2019-05-20 17:45:31 +0800

    :heavy_plus_sign: add mapstruct dependency

--- infrastructure/src/main/java/com/thoughtworks/ddd/sample/jingxi/infrastructure/inboundorder/mapper/InboundOrderMapper.java
+++ infrastructure/src/main/java/com/thoughtworks/ddd/sample/jingxi/infrastructure/inboundorder/mapper/InboundOrderMapper.java
@@ -2,7 +2,9 @@ package com.thoughtworks.ddd.sample.jingxi.infrastructure.inboundorder.mapper;
 
 import com.thoughtworks.ddd.sample.jingxi.domain.inboundorder.model.InboundOrder;
 import com.thoughtworks.ddd.sample.jingxi.infrastructure.inboundorder.po.InboundOrderPo;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface InboundOrderMapper {
 
     InboundOrder toDomain(InboundOrderPo po);
